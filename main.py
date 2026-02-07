3c873f93e9b4fbe18add28d21113349b39c3c87d
Yu Yu <hyuyu1544@gmail.com>
2026-02-07 11:47:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from notify import send_email_notification
 if __name__ == "__main__":
     print("正在檢查票券...")
     tickets = check_tickets()
+    # TODO: remove this
+    send_email_notification("WBC 2026 台灣賽事釋出票券！", "testing")
     if tickets:
         for ticket in tickets:
             send_email_notification("WBC 2026 台灣賽事釋出票券！", ticket)
